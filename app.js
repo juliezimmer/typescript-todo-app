f6ceb76fdf784b7aa3396497f6e98254e37484fa
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-12-14 13:59:12 -0600

    added interface, enums, getter, setters, and two classes to the app.

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var todo = "do laundry";
-var name = "Julie";
+
+constructor(private, todos, Todo[]);
+{
+}
+getAll();
+{
+    return this.todos;
+}
+var TodoState;
+(function (TodoState) {
+    TodoState[TodoState["New"] = 1] = "New";
+    TodoState[TodoState["Active"] = 2] = "Active";
+    TodoState[TodoState["Complete"] = 3] = "Complete";
+    TodoState[TodoState["Deleted"] = 4] = "Deleted";
+})(TodoState || (TodoState = {}));
--- app.ts
+++ app.ts
@@ -1,2 +1,70 @@
-let todo: string = "do laundry";
-let name: string = "Julie";
+class TodoService {
+    /*lastId assigns a new id to each todo added.
+    It is static because it needs to be the same throughout the app and may be used in more than one component. */
+    static lastId: number = 0;
+    constructor(private todos: Todo[]) {
+    }
+    
+    //the add method adds a new id to each new todo.
+    add(todo: Todo) {
+      //var newId is the unique id that is added to each todo.
+      var newId = TodoService.getNextId();
+      }
+
+    getAll() {
+      return this.todos;
+    } 
+
+    static getNextId() {
+      return TodoService.lastId += 1;
+    }
+}
+
+interface Todo {
+    name: string;
+    state: TodoState;
+}
+
+//this gives each of a task a number value. 
+enum TodoState {
+    New = 1,
+    Active,
+    Complete,
+    Deleted
+}
+
+/*this class holds the logic for the getter and setter and cZhanging the state of the todo. */
+class SmartTodo {
+    /* ._state is defined as TodoState, which is the enum TodoState */
+     _state: TodoState;
+    name: string;
+    
+    //using a getter
+    get state() {
+        return this._state;
+    }
+    
+    //using a setter 
+    set state(newState) {
+        /* restricts the state value that is set.
+        This logic evaluates the state and decides if the new setter can be assigned. */
+        if(newState == TodoState.Complete) {
+            var canBeCompleted = 
+                this.state == TodoState.Active
+                || this.state == TodoState.Deleted;
+                
+            if(!canBeCompleted) {
+                throw "Todo must be Active or Deleted in order to be marked Completed"
+            }
+        }
+        this._state = newState;
+    }
+      
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+  
+var todo = new SmartTodo("Pick up drycleaning");
+todo.state = TodoState.Complete;
+todo.state
